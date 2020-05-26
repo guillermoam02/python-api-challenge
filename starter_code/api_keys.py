ea05b429ae49cacace2ac33cae8b6e37367aaeac
Guillermo Avila <memoavila71@hotmail.com>
2020-05-25 22:42:06 -0500

    con correcciones de tarea

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = '4eaaaae07179024d3f945164f6b65da4'
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = 'AIzaSyD7HMZDK94Hc5uhVnRehysPgOg_1sMWw-I'
